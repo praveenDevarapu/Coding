ad3b0143a6b0c95c59393af0e7c877b0112ce5fd
praveen <96132928+praveenDevarapu@users.noreply.github.com>
2023-10-11 16:06:55 +0530

    Time: 312 ms (74.03%), Space: 18.6 MB (73.18%) - LeetHub

--- 0924-fair-candy-swap/0924-fair-candy-swap.py
+++ 0924-fair-candy-swap/0924-fair-candy-swap.py
@@ -2,11 +2,11 @@ class Solution:
     def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
         total_alice=sum(aliceSizes)
         total_bob=sum(bobSizes)
-        total_target=(total_alice+total_bob)//2
+        total_target=(total_alice-total_bob)//2
         alice_count=set(aliceSizes)
-        for size in bobSizes:
-            alice_req=total_target-(total_bob-size)
+        for b in bobSizes:
+            alice_req=b+total_target
             if alice_req in alice_count:
-                return [alice_req,size]
+                return [alice_req,b]
         return []
         
